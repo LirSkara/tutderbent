f50023143f37b15d994899837b7ba020cf427732
LirSkara <itcp199@gmail.com>
2022-02-04 16:34:51 +0300

    Full redisigned

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,79 +7,136 @@
             <h1 class="display-5 d-inline lh-1 mb-1 text-center">Тут Дербент</h1>
         </a>
     </div>
-    <div class="form-floating mt-3 mt-lg-5">
-        <input type="search" class="form-control border-success border-2 br-search" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput"><i class="bi bi-search"></i> Что будем искать?</label>
-    </div>
+    <form action="">
+        <div class="form-floating mt-3 mt-lg-5">
+            <input type="search" class="form-control border-success border-2 br-search" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput" class="small ms-2"><i class="bi bi-search"></i> Что будем искать?</label>
+        </div>
+    </form>
+            <ul class="d-flex list-unstyled mt-2 mb-1 py-1 scroll-none small hesh-buttons">
+                <li><a href="" class="text-dark text-decoration-none rounded-3 py-1 px-2 mx-1 d-flex">
+                        <span>Бейсболки</span>
+                    </a></li>
+                <li><a href="" class="text-dark bg-light text-decoration-none rounded-3 py-1 px-2 mx-1 d-flex">
+                        <span>Бомберы</span>
+                    </a></li>
+                <li><a href="" class="text-dark bg-light text-decoration-none rounded-3 py-1 px-2 mx-1 d-flex">
+                        <span>Ботинки</span>
+                    </a></li>
+                <li><a href="" class="text-dark bg-light text-decoration-none rounded-3 py-1 px-2 mx-1 d-flex">
+                        <span>Брюки</span>
+                    </a></li>
+                <li><a href="" class="text-dark bg-light text-decoration-none rounded-3 py-1 px-2 mx-1 d-flex">
+                        <span>Варежки</span>
+                    </a></li>
+                <li><a href="" class="text-dark bg-light text-decoration-none rounded-3 py-1 px-2 me-2 d-flex">
+                        <span class="me-1">Варежки</span>
+                    </a></li>
+                <li><a href="" class="text-dark bg-light text-decoration-none rounded-3 py-1 px-2 me-2 d-flex">
+                        <span>Ветровки</span>
+                    </a></li>
+            </ul>
+            <div class="row g-2 small">
+                <div class="col-6 col-lg-3">
+                    <div class="bg-light rounded-3">
+                        <div class="row ps-3">
+                            <div class="col-6 fs-3 pe-0 pt-1">
+                                <i class="bi bi-calendar-week text-danger"></i> <span class="fs-2">4</span>
+                            </div>
+                            <div class="col small py-1 px-0">
+                                <span class="fw-bold">Февраля</span><br>
+                                Пятница
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3">
+                    <div class="bg-light rounded-3">
+                        <div class="row ps-2">
+                            <div class="col-6 pt-1 fs-3 pe-0">
+                                <img src="https://yastatic.net/weather/i/icons/islands/32/bkn-d.png" alt="..." width="30"> +5°
+                            </div>
+                            <div class="col small py-1 ps-1">
+                                Днем +6°<br>
+                                Завтра +5°
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <ul class="row row-cols-4 px-0 py-0 my-3 text-center mx-0 rounded-3 small">
+                    <li class="col px-0 list-group">
+                        <a href="/page/catalog.php" class="text-decoration-none link-dark">
+                            <i class="bi bi-book text-primary d-block fs-3"></i>Каталог
+                        </a>
+                    </li>
+                    <li class="col px-0 list-group">
+                        <a href="/page/news.php" class="text-decoration-none link-dark">
+                            <i class="bi bi-newspaper text-info d-block fs-3"></i>Новости
+                        </a>
+                    </li>
+                    <li class="col px-0 list-group">
+                        <a href="/page/ads.php" class="text-decoration-none link-dark">
+                            <i class="bi bi-layout-text-sidebar text-warning d-block fs-3"></i>Объявления
+                        </a>
+                    </li>
+                    <li class="col px-0 list-group">
+                        <a href="/page/market.php" class="text-decoration-none link-dark">
+                            <i class="bi bi-shop text-danger d-block fs-3"></i>Маркет
+                        </a>
+                    </li>
+                </ul>
+            </div>
 </div>
 
 <div class="bg-light pt-1 pb-3 mt-3">
-<div class="container col-xxl-7 px-3">
-    <div class="shadow bg-white border rounded-3 mt-2">
-        <div>
-            <div class="row mb-2 mt-2 mx-1 border-bottom pb-2">
-                <div class="col">
-                <span class="py-1 text-dark text-decoration-none">
-                    <i class="bi bi-list"></i> Главное меню
-                </span>
+<div class="container col-xxl-7 px-0">
+    <div class="row row-cols-1 row-cols-lg-2 mx-0">
+        <div class="col">
+            <div class="shadow bg-white border rounded-3 mt-2">
+                <img src="https://tutderbent.ru/media/images/news/967302825.jpg" class="img-fluid rounded-top" alt="Дербенту придали особый статус">
+                <div class="text-dark px-3 pt-2">
+                    <div class="mb-3">
+                        <a href="page/new.php?id=12" class="text-decoration-none link-dark">
+                            <div class="fs-5 mb-1"><i class="bi bi-newspaper text-info me-1"></i> Дербенту придали статус</div>
+                            <p class="display-6 fs-6">Закон «Об особом статусе города Дербента как исторического и культурного центра Республики Дагестан» приняли депутаты дагестанского парламента на 56-сессии во втором окончательном чтении.</p>
+                        </a>
+                        <div>
+                            <span class="bg-light btn">3 Февраля 2022 г.</span>
+                            <span class="bg-light btn"><i class="bi bi-person"></i> Admin</span>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <ul class="row row-cols-4 px-0 py-0 my-3 text-center mx-0 rounded-3 small">
-                <li class="col px-0 list-group">
-                    <a href="/page/catalog.php" class="text-decoration-none link-dark">
-                        <i class="bi bi-book text-primary d-block fs-3"></i>Каталог
-                    </a>
-                </li>
-                <li class="col px-0 list-group">
-                    <a href="/page/news.php" class="text-decoration-none link-dark">
-                        <i class="bi bi-newspaper text-info d-block fs-3"></i>Новости
-                    </a>
-                </li>
-                <li class="col px-0 list-group">
-                    <a href="/page/ads.php" class="text-decoration-none link-dark">
-                        <i class="bi bi-layout-text-sidebar text-warning d-block fs-3"></i>Объявления
-                    </a>
-                </li>
-                <li class="col px-0 list-group">
-                    <a href="/page/market.php" class="text-decoration-none link-dark">
-                        <i class="bi bi-shop text-danger d-block fs-3"></i>Маркет
-                    </a>
-                </li>
-            </ul>
         </div>
-    </div>
-</div>
-<div class="container col-xxl-7 px-3">
-<div class="shadow bg-white rounded-3 p-2 mt-3">
-    <div>
 
-    <div class="row mb-2 mt-1 px-1">
         <div class="col">
-        <a href="" class="px-2 py-1 text-dark text-decoration-none">
-            <i class="bi bi-newspaper"></i> Новости сейчас
-        </a>
-        </div>
-
-        <div class="col-4 text-end">
-        </div>
-    </div>
-
-    <div class="text-dark px-2">
-        <ul class="nav nav-pills small" id="myTab" role="tablist">
-        <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Последняя новость</button>
-        </li>
-        </ul>
-        <div class="tab-content mt-3 px-1" id="myTabContent">
-        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-            <div class="mb-3"><a href="page/new.php?id=12" class="text-decoration-none link-dark"><h5 class="fw-normal">Дербенту придали особый статус</h5><p class="display-6 fs-6">Закон «Об особом статусе города Дербента как исторического и культурного центра Республики Дагестан» приняли депутаты дагестанского парламента на 56-сессии во втором окончательном чтении.</p></a>
+            <div class="shadow bg-white border rounded-3 mt-2">
+                <img src="https://tutderbent.ru/media/images/list/1460494246.jpeg" class="img-fluid rounded-top" alt="Дербенту придали особый статус">
+                <div class="text-dark px-3 pt-2">
+                    <div class="mb-3">
+                        <div>
+                            <span class="bg-light btn">
+                                <span class="text-warning small">
+                                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                                </span>
+                            <span class="ms-1">5</span>
+                            </span>
+                            <span class="bg-light btn"><i class="bi bi-chat"></i> 5</span>
+                            <span class="bg-light btn"><i class="bi bi-image"></i> 12</span>
+                        </div>
+                        <a href="page/new.php?id=12" class="text-decoration-none link-dark">
+                            <div class="fs-5 mb-1"><i class="bi bi-book text-primary me-1"></i> Рандеву Ресторан</div>
+                            <p class="mb-2 text-muted"><i class="bi bi-geo-alt-fill"></i> Г.Дербент ул.Гагарина 10</p>
+                            <p class="display-6 fs-6">Lounge-Cafe “Randevu” - это сочетание Европейской, Японской и Кавказской кухни во вкуснейших блюдах</p>
+                        </a>
+                    </div>
+                </div>
             </div>
-
         </div>
-        </div>
+    </div>
 
-    </div>
-    </div>
 </div>
-</div></div>
+</div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,7 +17,7 @@
 {{-- Навбар --}}
 <nav class="mt-2">
   <div class="container-fluid d-flex flex-wrap">
-    <ul class="nav me-auto">
+    <ul class="nav me-auto small">
 
     {{-- <li class="dropdown">
       <button class="btn bg-light border border-custom me-1 py-2 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
@@ -28,21 +28,48 @@
         <li><a class="dropdown-item" href="">Русский</a></li>
       </ul>
     </li> --}}
-      <li class="nav-item bg-light rounded border border-custom px-1 me-1">
-        <a href="tel:+79618888837" class="nav-link link-dark px-2"><i class="bi bi-telephone"></i></a>
+      <li class="nav-item bg-light btn border px-1 me-1">
+        <a href="tel:+79618888837" class="link-dark px-2"><i class="bi bi-telephone"></i></a>
       </li>
-      <li class="nav-item bg-light rounded border border-custom px-1 me-1">
-        <a href="#" class="nav-link link-dark px-2"><i class="bi bi-instagram"></i></a>
+      <li class="nav-item bg-light btn border px-1 me-1">
+        <a href="#" class="link-dark px-2"><i class="bi bi-instagram"></i></a>
       </li>
-      <li class="nav-item bg-light border rounded border-custom px-1">
-        <a href="" class="nav-link link-dark px-2"><i class="bi bi-whatsapp"></i></a>
+      <li class="nav-item bg-light btn border px-1">
+        <a href="" class="link-dark px-2"><i class="bi bi-whatsapp"></i></a>
       </li>
     </ul>
     <ul class="nav">
-      <li class="nav-item"><a data-bs-toggle="modal" data-bs-target="#sign" href="#" class="btn btn-lg bg-success text-white border-custom px-2 py-1"><i class="bi bi-box-arrow-in-right"></i> Войти</a></li>
+      <li class="nav-item"><a data-bs-toggle="modal" data-bs-target="#sign" href="#" class="btn bg-success text-white border-custom px-2 py-1"><i class="bi bi-box-arrow-in-right"></i> Войти</a></li>
     </ul>
   </div>
 </nav>
   @yield('content')
+  <nav class="navbar navbar-light">
+    <div class="container pt-3">
+      <div class="btn-group dropup">
+        <button class="btn bg-light border border-custom dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="bi bi-globe"></i> Русский
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+        <li><a class="dropdown-item" href="/system/handler.php?c_lng=en&amp;l=https://tutderbent.ru/">English</a></li>
+        <li><a class="dropdown-item" href="/system/handler.php?c_lng=ru&amp;l=https://tutderbent.ru/">Русский</a></li>
+        </ul>
+      </div>
+      <div class="d-flex">
+        <ul class="nav mx-auto pt-0">
+        <li class="nav-item bg-light border btn border-custom px-1 me-1">
+        <a href="tel:+79618888837" class="link-dark px-2 py-1"><i class="bi bi-telephone"></i></a>
+      </li>
+      <!-- <li class="nav-item bg-gray border-custom px-1 me-1">
+        <a href="#" class="nav-link link-dark px-2 py-1"><i class="bi bi-instagram"></i></a>
+      </li> -->
+      <li class="nav-item bg-light border btn border-custom px-1">
+        <a href="https://wa.me/79618888837?text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82!%20%F0%9F%91%8B%20%D0%AF%20%D0%BF%D0%B8%D1%88%D1%83%20%D0%B2%D0%B0%D0%BC%20%D1%81%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0%20%D0%A2%D1%83%D1%82%20%D0%94%D0%B5%D1%80%D0%B1%D0%B5%D0%BD%D1%82." class="link-dark px-2 py-1"><i class="bi bi-whatsapp"></i></a>
+      </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="text-center text-muted mb-4">© 2021 Тут Дербент</div>
 </body>
 </html>
